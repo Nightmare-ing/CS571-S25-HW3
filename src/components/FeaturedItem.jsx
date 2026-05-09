fda60107aeb8ce173131b5f7d947b8dffa46481f
Qingsong Hu <kingsanhu@gmail.com>
2026-05-09 20:33:45 +0800

    Finish part 3 of HW3
    Add a button, which can be flipped to show different contents

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function FeaturedItem(props) {
+    const [flip, setFlip] = useState(false);
+
+    const clickToFlip = () => {
+        setFlip(!flip);
+    };
+
     return (
         <div>
             <img src={props.img} alt={props.name} />
             <h2>{props.name}</h2>
             <p>${props.price} per unit</p>
             <p>{props.description}</p>
+            <button onClick={clickToFlip}>
+                {flip ? "Hide Nutrition Facts" : "Show Nutrition Facts"}
+            </button>
         </div>
     );
 }
