194c41a8e819b19e7f462670d3440fa7d3000c46
Qingsong Hu <kingsanhu@gmail.com>
2026-05-09 20:46:40 +0800

    Finish part 4 of HW
    When the button is flipped, show a table about nutrition.
    If some nutrition fact item is missing, show default "0g"

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -13,6 +13,26 @@ export default function FeaturedItem(props) {
             <h2>{props.name}</h2>
             <p>${props.price} per unit</p>
             <p>{props.description}</p>
+            {flip ?
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Calories</th>
+                            <th>Fat</th>
+                            <th>Carbohydrates</th>
+                            <th>Protein</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{props.nutrition.calories ?? "0g"}</td>
+                            <td>{props.nutrition.fat ?? "0g"}</td>
+                            <td>{props.nutrition.carbohydrates ?? "0g"}</td>
+                            <td>{props.nutrition.protein ?? "0g"}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            :   <div></div>}
             <button onClick={clickToFlip}>
                 {flip ? "Hide Nutrition Facts" : "Show Nutrition Facts"}
             </button>
